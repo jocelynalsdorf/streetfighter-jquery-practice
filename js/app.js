26a1c3d91d28078d0f415133b3982fd39b9976ec
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-12 12:04:06 +0700

    add audio

--- js/app.js
+++ js/app.js
@@ -8,10 +8,10 @@ $(document).ready(function() {
 		$(".ryu-still").show();
 	})
 	.mousedown(function() {
-		//play hadouken sound
+		playHadouken();
 		$(".ryu-ready").hide();
 		$(".ryu-throwing").show();
-		$(".hadouken").show();
+		$(".hadouken").finish().show();
 		$(".hadouken").animate(
       {'left': '300px'},
       500,
@@ -26,6 +26,12 @@ $(document).ready(function() {
 		$(".ryu-ready").show();
 		$(".ryu-throwing").hide();
 	});
+
+
+	function playHadouken () {
+		$("#hadouken-sound")[0].volume = 0.5;
+		$("#hadouken-sound")[0].load();
+		$("#hadouken-sound")[0].play();
+	}
 });
 
-//.hadouken 
